f9293679dad8a06b1c8fae4b40f3cf4a23e0091a
= <=>
2024-11-27 23:15:07 +0700

    convert RequestMapping into Post and Get

--- src/main/java/vn/laptopshop/laptopshop/controller/UserController.java
+++ src/main/java/vn/laptopshop/laptopshop/controller/UserController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import vn.laptopshop.laptopshop.domain.User;
 import vn.laptopshop.laptopshop.service.UserService;
@@ -22,22 +20,23 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
+    // redirect: là kiểu F5 khi POST and delete
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String getHomePage(Model model) {
         List<User> arrUsers = this.userService.getAllUserByEmail("1@gmail.com");
         System.out.println(arrUsers);
         return "hello";
     }
 
-    @RequestMapping("/admin/user")
+    @GetMapping("/admin/user")
     public String getUserPage(Model model) {
         List<User> users = this.userService.getAllUser();
         model.addAttribute("users1", users);
         return "admin/user/table-user";
     }
 
-    @RequestMapping("/admin/user/{id}")
+    @GetMapping("/admin/user/{id}")
     public String getUserDetailPage(Model model, @PathVariable long id) {
         User user = this.userService.getUserById(id);
         model.addAttribute("user", user);
@@ -60,20 +59,20 @@ public class UserController {
         return "redirect:/admin/user"; // Redirect về danh sách người dùng sau khi cập nhật
     }
 
-    @RequestMapping("/admin/user/update/{id}")
+    @GetMapping("/admin/user/update/{id}")
     public String getUpdateUserPage(Model model, @PathVariable long id) {
         User currentUser = this.userService.getUserById(id);
         model.addAttribute("newUser", currentUser);
         return "admin/user/update";
     }
 
-    @RequestMapping("/admin/user/create") // GET
+    @GetMapping("/admin/user/create") // GET
     public String getCreateUserPage(Model model) {
         model.addAttribute("newUser", new User());
         return "admin/user/create";
     }
 
-    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
+    @PostMapping("/admin/user/create")
     public String getUserPage(Model model, @ModelAttribute("newUser") User user) {
         this.userService.handleSaveUser(user);
         return "redirect:/admin/user";
