234c980d9b186df3ab6611da6676baa71dd2a55a
= <=>
2024-12-19 16:55:04 +0700

    CRUD Product complete

--- src/main/java/vn/laptopshop/laptopshop/service/ProductService.java
+++ src/main/java/vn/laptopshop/laptopshop/service/ProductService.java
@@ -1,6 +1,7 @@
 package vn.laptopshop.laptopshop.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -9,6 +10,7 @@ import vn.laptopshop.laptopshop.repository.ProductRepository;
 
 @Service
 public class ProductService {
+
     private final ProductRepository productRepository;
 
     public ProductService(ProductRepository productRepository) {
@@ -22,4 +24,13 @@ public class ProductService {
     public List<Product> fetchProducts() {
         return this.productRepository.findAll();
     }
+
+    public Optional<Product> fetchProductById(long id) {
+        return this.productRepository.findById(id);
+    }
+
+    public void deleteProduct(long id) {
+        this.productRepository.deleteById(id);
+    }
+
 }
