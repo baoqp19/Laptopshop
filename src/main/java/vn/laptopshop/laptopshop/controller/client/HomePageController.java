cbb647e2db0c7af3821e49cf3bcc0abad19d12f4
= <=>
2024-12-19 21:24:18 +0700

    Load product dynamic

--- src/main/java/vn/laptopshop/laptopshop/controller/client/HomePageController.java
+++ src/main/java/vn/laptopshop/laptopshop/controller/client/HomePageController.java
@@ -1,13 +1,27 @@
 package vn.laptopshop.laptopshop.controller.client;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import vn.laptopshop.laptopshop.domain.Product;
+import vn.laptopshop.laptopshop.service.ProductService;
 
 @Controller
 public class HomePageController {
 
+    private final ProductService productService;
+
+    public HomePageController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping("/")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
+        List<Product> products = this.productService.fetchProducts();
+        model.addAttribute("products", products);
         return "client/homepage/show";
     }
 
