a56c513a936d45855a79e0390238e78c3dc7897d
= <=>
2025-01-01 16:14:35 +0700

    load dynamic Cart and Increase and decrease

--- src/main/java/vn/laptopshop/laptopshop/controller/client/ItemController.java
+++ src/main/java/vn/laptopshop/laptopshop/controller/client/ItemController.java
@@ -1,5 +1,8 @@
 package vn.laptopshop.laptopshop.controller.client;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +11,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import vn.laptopshop.laptopshop.domain.Cart;
+import vn.laptopshop.laptopshop.domain.CartDetail;
 import vn.laptopshop.laptopshop.domain.Product;
+import vn.laptopshop.laptopshop.domain.User;
 import vn.laptopshop.laptopshop.service.ProductService;
 
 @Controller
@@ -41,7 +47,24 @@ public class ItemController {
     }
 
     @GetMapping("/cart")
-    public String getCartPage(Model model) {
+    public String getCartPage(Model model, HttpServletRequest request) {
+        User currentUser = new User();// null
+        HttpSession session = request.getSession(false);
+        long id = (long) session.getAttribute("id");
+        currentUser.setId(id);
+
+        Cart cart = this.productService.fetchByUser(currentUser);
+
+        List<CartDetail> cartDetails = cart == null ? new ArrayList<CartDetail>() : cart.getCartDetails();
+
+        double totalPrice = 0;
+        for (CartDetail cd : cartDetails) {
+            totalPrice += cd.getPrice() * cd.getQuantity();
+        }
+
+        model.addAttribute("cartDetails", cartDetails);
+        model.addAttribute("totalPrice", totalPrice);
+
         return "client/cart/show";
     }
 }
